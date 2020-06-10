28be5245be3b90bc52fd8c1963f3842e6cc64764
epicavic <epicavic@gmail.com>
2020-06-10 10:25:10 +0300

    ch4: add text template

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -33,13 +34,32 @@ type user struct {
 }
 
 func main() {
+	const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+	var report = template.Must(template.New("issuelist").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ))
+
 	result, err := searchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+
+	fmt.Println("========================================")
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -70,4 +90,8 @@ func searchIssues(terms []string) (*issuesSearchResult, error) {
 	}
 
 	return &result, nil
+}
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
 }
