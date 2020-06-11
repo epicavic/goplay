b06eefb2c6720f586f1d1c334e86a94a15ee56e3
epicavic <epicavic@gmail.com>
2020-06-11 09:30:06 +0300

    ch5: ref types

---
+++ ch5/ref/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4} //slice
+	b := 1                 //int
+	c := &b                //pointer
+	d := map[string]int{   //map
+		"alice": 1,
+		"bob":   2,
+	}
+
+	// Arguments are passed by value, so the function receives a copy
+	// of each argument, modifications to the copy do not affect the caller.
+	// However, if the argument contains some kind of reference,
+	// like a pointer, slice, map, function, or channel, then the caller
+	// may be affected by any modifications the function makes to variables
+	// indirectly referred to by the argument.
+
+	adda(a, b)
+	fmt.Println(a) // returns "[2 2 3 4]" : reference type - changed
+	addb(b, b)
+	fmt.Println(b) // returns "1" : integer type - not changed
+	modc(c)
+	fmt.Println(c, *c, b) // returns "0xc00009e068 2 2" : reference type - changed
+	addd(d)
+	fmt.Println(d) // returns "map[alice:2 bob:3]" : reference type - changed
+}
+
+func adda(aaa []int, b int) []int {
+	aaa[0] += b
+	return aaa
+}
+
+func addb(bbb, b int) int {
+	bbb += b
+	return bbb
+}
+
+func modc(ccc *int) *int {
+	*ccc = 2
+	return ccc
+}
+
+func addd(ddd map[string]int) map[string]int {
+	for k := range ddd {
+		ddd[k]++
+	}
+	return ddd
+}
