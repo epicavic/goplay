325d0621ebba8ee78391a1b0a3bcc6ed974b0b06
epicavic <epicavic@gmail.com>
2020-07-09 10:50:17 +0300

    ch7: add http1

---
+++ ch7/http1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// define types
+type dollars float64
+type database map[string]dollars
+
+// define methods to satisfy fmt.Stringer and http.Handler interfaces
+func (d dollars) String() string { return fmt.Sprintf("$ %2.2f", d) }
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func main() {
+	db := database{"pants": 20, "shirts": 10}
+	log.Fatal(http.ListenAndServe("localhost:8080", db))
+}
