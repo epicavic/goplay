567556d9db49b5b01031ededcf59b66affecfa59
epicavic <epicavic@gmail.com>
2020-07-30 11:26:24 +0300

    all: add license info

---
+++ license.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+header='// Derivative of "The Go Programming Language" © 2016 examples by
+// Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+'
+dir=${1:-.}
+tmp=$(mktemp)
+
+for file in $(find ${dir} -type f -not -path '*/\.*'); do
+  if ! grep -q 'Derivative of "The Go Programming Language"' ${file}; then
+    {
+    echo "$header"
+    cat $file
+    } > $tmp
+    mv $tmp $file
+  fi
+done
+rm -f $tmp
